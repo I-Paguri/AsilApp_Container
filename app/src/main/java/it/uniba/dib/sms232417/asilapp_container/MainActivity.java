1aa17f368647172339aea0639191b087ebb6f00a
SergioPinto <38921333+SergioPinto2501@users.noreply.github.com>
2023-12-23 12:22:16 +0100

    Import Project

---
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp_container/MainActivity.java
@@ -0,0 +1,14 @@
+package it.uniba.dib.sms232417.asilapp_container;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
