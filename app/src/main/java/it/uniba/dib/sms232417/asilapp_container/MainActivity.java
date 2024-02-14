bf866afc8d4868ad40a98e52e20246331f995533
SergioPinto <38921333+SergioPinto2501@users.noreply.github.com>
2024-02-14 12:47:41 +0100

    Thread for CheckConnection

--- app/src/main/java/it/uniba/dib/sms232417/asilapp_container/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp_container/MainActivity.java
@@ -1,19 +1,42 @@
 package it.uniba.dib.sms232417.asilapp_container;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import it.uniba.dib.sms232417.asilapp_container.thread_connection.InternetCheckThread;
 import it.uniba.dib.sms232417.asilapp_container.welcome_fragment.QRCodeAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+                if (msg.what == 0) {
+                    showNoInternetDialog();
+                }
+                return true;
+            }
+        });
+        InternetCheckThread internetCheckThread = new InternetCheckThread(this, handler);
+        internetCheckThread.start();
         setContentView(R.layout.activity_main);
         replaceFragment(new QRCodeAuth());
 
@@ -38,4 +61,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    private void showNoInternetDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Please check your internet connection and try again");
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+        });
+        builder.show();
+    }
 }
+
---
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp_container/thread_connection/InternetCheckThread.java
@@ -0,0 +1,43 @@
+package it.uniba.dib.sms232417.asilapp_container.thread_connection;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.util.Log;
+
+public class InternetCheckThread extends Thread {
+    private Context context;
+    boolean isDialogShow = false;
+    private Handler handler;
+    public InternetCheckThread(Context context, Handler handler) {
+        this.context = context;
+        this.handler = handler;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+                if (isConnected) {
+                    Log.d("Internet", "Connected");
+                    isDialogShow = false;
+                } else {
+                    Log.d("Internet", "Not connected");
+                    if(!isDialogShow) {
+                        handler.sendEmptyMessage(0);
+                        isDialogShow = true;
+                    }
+
+
+                }
+                // Sleep for 5 seconds before checking again
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
